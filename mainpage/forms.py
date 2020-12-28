cb836f28156587cd04a4b41959daceb783bc4d61
Seunghan Yoon <dbstmdgks93@naver.com>
2020-12-28 20:28:33 +0900

    second commit

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import CustomUser
 
 class SignUpForm(UserCreationForm):
     name = forms.CharField(max_length=20, help_text='input your name')
@@ -8,5 +9,5 @@ class SignUpForm(UserCreationForm):
     gender = forms.CharField(max_length=20, help_text='input your gender')
 
     class Meta(UserCreationForm.Meta):
-        model = User
+        model = CustomUser
         fields = UserCreationForm.Meta.fields + ('name','birth','gender')
